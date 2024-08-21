367fbc35dff38456a689f59824d0ee6c3878eaf1
skokovin <s.kokovin@gmail.com>
2024-08-21 20:31:18 +0300

    enable select

---
+++ src/app/model/pipe-bend-cnc.spec.ts
@@ -0,0 +1,7 @@
+import { PipeBendCnc } from './pipe-bend-cnc';
+
+describe('PipeBendCnc', () => {
+  it('should create an instance', () => {
+    expect(new PipeBendCnc()).toBeTruthy();
+  });
+});
---
+++ src/app/model/pipe-bend-cnc.ts
@@ -0,0 +1,22 @@
+export class PipeBendCnc {
+  id1: number;
+  id2: number;
+  l: number;
+  lt: number;
+  r: number;
+  a: number;
+  clr: number;
+  outd: number;
+
+
+  constructor(id1: number, id2: number, l: number, lt: number, r: number, a: number, clr: number, outd: number) {
+    this.id1 = id1;
+    this.id2 = id2;
+    this.l = l;
+    this.lt = lt;
+    this.r = r;
+    this.a = a;
+    this.clr = clr;
+    this.outd = outd;
+  }
+}
